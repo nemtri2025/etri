26090078f75b3bdbfcceb291a373a1d2443d5db5
unknown <nemtri@yandex.com>
2025-01-20 20:49:10 -0800

    first version of etri-webpage porfolia to check domain adding

---
+++ src/components/services.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export const Services = (props) => {
+  return (
+    <div id="services" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>Разработки</h2>
+          <p>
+          Наш подход основан на глубоком научном понимании и стремлении к созданию решений, расширяющих границы возможностей человека, улучшая качество жизни и обеспечивая новый уровень взаимодействия с технологиями
+          </p>
+        </div>
+        <div className="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-4">
+                  {" "}
+                  <i className={d.icon}></i>
+                  <div className="service-desc">
+                    <h3>{d.name}</h3>
+                    <p>{d.text}</p>
+                  </div>
+                </div>
+              ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
